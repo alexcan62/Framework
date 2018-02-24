526a3d4850574de8ee271591236b899a804803b8
alexcan62 <alexcan62@free.fr>
2018-02-24 14:33:13 +0100

    first commit

---
+++ config/app.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexc
+ * Date: 24/02/2018
+ * Time: 14:32
+ */
+
+return [
+
+    /**
+     * Name of your application (eg. Framework)
+     */
+    "name" => "Framework",
+
+];
