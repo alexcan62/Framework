c74fb80e79edad45382b3cab55e70e65f7b3e9f7
alexcan62 <alexcan62@free.fr>
2018-02-24 17:22:45 +0100

    first commit

--- config/app.php
+++ config/app.php
@@ -13,4 +13,24 @@ return [
      */
     "name" => "Framework",
 
+    /**
+     * Environment (eg. prod for production)
+     */
+    "env" => "local",
+
+    /**
+     * Debug mode on your app (warning, default: true)
+     */
+    "debug" => true,
+
+    /**
+     * Domain name, eg. https://acanoen.fr/ or http://localhost/
+     */
+    "domain" => "http://localhost/",
+
+    /**
+     * Security key with its encoder (eg. base64:key_generated)
+     */
+    "key" => "base64:u1&78l/qP0GsT2M8q,(Ru{NU(@w6Kd+6",
+
 ];
