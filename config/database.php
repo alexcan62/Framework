b83a536cf024a44e1c8338a83d67fd8b6dfbaf0b
alexcan62 <alexcan62@free.fr>
2018-02-25 13:22:25 +0100

    first commit

---
+++ config/database.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexc
+ * Date: 25/02/2018
+ * Time: 13:17
+ */
+
+return [
+
+    /**
+     * Engine of your database (eg. MySQL or SQLite)
+     */
+    "engine" => "mysql",
+
+    /**
+     * Host and its communication port (eg. localhost:3306)
+     */
+    "host" => "localhost:3306",
+
+    /**
+     * Name of the database
+     */
+    "name" => "framework",
+
+    /**
+     * Authenticate user with its password
+     */
+    "auth" => [
+        "username" => "root",
+        "password" => "root",
+    ],
+
+    /**
+     * Encoding of characters (eg. utf8_general_ci)
+     */
+    "encoding" => "utf8_general_ci",
+
+];
---
+++ config/mail.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alexc
+ * Date: 25/02/2018
+ * Time: 13:13
+ */
+
+return [
+
+    /**
+     * Your mail server (eg. mail.ovh.net)
+     */
+    "host" => "127.0.0.1",
+
+    /**
+     * Your mailing port (eg. 25)
+     */
+    "port" => "1025",
+
+    /**
+     * Security of the transmission (eg. tls)
+     */
+    "security" => null,
+
+    /**
+     * Authenticate user with its password
+     */
+    "auth" => [
+        "username" => null,
+        "password" => null,
+    ],
+
+];
